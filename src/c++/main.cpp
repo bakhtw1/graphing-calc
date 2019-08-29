a88b01d42c5d09eb5867b118914c3253f5c8b7fd
Waqas bakht <waqasbakht@Waqass-Air.phub.net.cable.rogers.com>
2019-08-28 23:25:05 -0400

    Add C++ configuration to vsc project settings

---
+++ src/c++/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main ( int argc, char *argv[] ) {
+
+    cout << "\n";
+    cout << "HELLO:\n";
+    cout << "  C++ version.\n";
+    cout << "\n";
+    cout << "  Hello, world!\n";
+
+    return 0;
+    
+}
